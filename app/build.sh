3eb5838b131bd699fa5aaf615162962fa86987a7
steve amancha <myc0mnsense@gmail.com>
2023-10-18 20:43:19 -0700

    finished merging Jeremy's branch and some eda for the df

---
+++ app/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -o errexit
+
+pip install -upgrade pip
+pip install -r requirements.txt
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-pip install -upgrade pip
-pip install -r requirements.txt
